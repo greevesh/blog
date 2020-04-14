758b3e453b989ac41d063293b0877297feb9a117
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-04-14 12:12:46 +0800

    The correct files are now being imported.

--- resources/bootstrap.php
+++ resources/bootstrap.php
@@ -1,6 +1,3 @@
 <?php
 
-require '../routes.php'; 
-require 'core/request.php'; 
-require 'views/partials/nav.view.php';
-require 'controllers/PagesController.php'; 
+require '../views/partials/nav.view.php'; 
